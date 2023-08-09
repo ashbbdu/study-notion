299050fad321752223c60b60aea3e7c6f7c211e9
Ashish Srivastava <ashishsrivastava@Ashishs-MacBook-Pro.local>
2023-08-09 10:35:36 +0530

    made some changes in config

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -4,7 +4,8 @@ import { BsChevronDown } from "react-icons/bs"
 import { useSelector } from "react-redux"
 import { Link, matchPath, useLocation } from "react-router-dom"
 
-import logo from "../../assets/Logo/Logo-Full-Light.png"
+// import logo from "../../assets/Logo/Logo-Full-Light.png"
+import logo1  from "../../assets/Logo/logo0099.png"
 import { NavbarLinks } from "../../data/navbar-links"
 import { apiConnector } from "../../services/apiconnector"
 import { categories } from "../../services/apis"
@@ -49,7 +50,7 @@ function Navbar() {
       <div className="flex w-11/12 max-w-maxContent items-center justify-between">
         {/* Logo */}
         <Link to="/">
-          <img src={logo} alt="Logo" width={160} height={32} loading="lazy" />
+          <img src={logo1} alt="Logo" width={280} height={32} loading="lazy" />
         </Link>
         {/* Navigation links */}
         <nav className="hidden md:block">
