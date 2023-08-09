f79505071fdbe11cf753b285b5f03c987e28ea00
Ashish Srivastava <ashishsrivastava@Ashishs-MacBook-Pro.local>
2023-08-09 13:55:00 +0530

    updates in email templates BE

--- server/mail/templates/emailVerificationTemplate.js
+++ server/mail/templates/emailVerificationTemplate.js
@@ -67,11 +67,11 @@ const otpTemplate = (otp) => {
 	<body>
 		<div class="container">
 			<a href="http://localhost:3000"><img class="logo"
-					src="https://i.ibb.co/grDz42m/upskill-yourself-logo-1.png" alt="StudyNotion Logo"></a>
+					src="https://i.ibb.co/grDz42m/upskill-yourself-logo-1.png" alt=" Logo"></a>
 			<div class="message">OTP Verification Email</div>
 			<div class="body">
 				<p>Dear User,</p>
-				<p>Thank you for registering with StudyNotion. To complete your registration, please use the following OTP
+				<p>Thank you for registering with Upskill Yourself. To complete your registration, please use the following OTP
 					(One-Time Password) to verify your account:</p>
 				<h2 class="highlight">${otp}</h2>
 				<p>This OTP is valid for 5 minutes. If you did not request this verification, please disregard this email.
